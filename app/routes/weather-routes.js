13c44da2d2eca253a6636b41b0d44afd69bbefef
Raymond Huang <huangrayr@gmail.com>
2013-03-23 21:55:37 -0700

    Changed base route.

--- app/routes/weather-routes.js
+++ app/routes/weather-routes.js
@@ -2,7 +2,7 @@
  * The route for the UW weather API located at http://api.uwaterloo.ca/#!/weather.
  */
 module.exports = function(app) {
-    app.get('/weather', function(req, res) {
+    app.get('/apis/weather', function(req, res) {
         require(__dirname + '/../models/weather').get(req, res, function(data) {
             res.send(data);
         });
