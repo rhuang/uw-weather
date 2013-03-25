cd8d24a59a8ec17bf90a74341cfd2cbad44355c3
Rahul Bhattacharya <r3bhatta@gmail.com>
2013-03-24 19:40:02 -0700

    Added initial layout of backbone structure

--- public/js/index.js
+++ public/js/index.js
@@ -1,30 +1,91 @@
+
 window.WeatherModel = Backbone.Model.extend({
-	urlRoot : "",
+	urlRoot : "apis/weather",
 	defaults : {
-		"Date" : "",
-		"Day" : "",
-		"AsOf" : "",
-		"Condition" : "",
-		"Icon" : "",
-		"Temp" : "",
-		"Humidity" : "",
-		"WindChill" : "",
-		"Max" : "",
-		"Min" : "",
-		"Wind" : "",
-		"WindDr" : "",
-		"Precipitation" : "",
-		"Radiation" : "",
-		"High" : "",
-		"Low" : ""
+		"date" 			: null,
+		"day"  			: null,
+		"asOf" 			: null,
+		"condition" 	: null,
+		"icon" 			: null,
+		"temp" 			: null,
+		"humidity" 		: null,
+		"windchill" 	: null,
+		"max" 			: null,
+		"min" 			: null,
+		"wind" 			: null,
+		"winddirection" : null,
+		"precipitation" : null,
+		"radiation" 	: null,
+		"high" 			: null,
+		"low" 			: null
+	}
+
+	/*
+
+	parse : function() {
+		WeatherModel wm = new WeatherModel({
+			date: reponse.date,
+			day: response.current.day
+		});
+		return wm;
+	}
+	*/
+});
+
+window.ForecastModel = Backbone.Model.extend({
+	defaults : {
+		"Condition" : null,
+		"Image" 	: null,
+		"High" 		: null,
+		"Low" 		: null  
 	}
 });
 
 window.WeatherCollection = Backbone.Collection.extend({
-	Model : WeatherModel,
-	URL : ""
+	model : ForecastModel,
+	url : "apis/weather",
+
+	parse: function(response){
+		return response.response.data.Week.result;
+	}
 });
 
+
+window.WeatherView = Backbone.Collection.extend({
+
+   // template:_.template($('#').html()),
+
+    initialize:function () {
+    },
+
+    render:function (eventName) {
+    } 
+});
+
+window.GraphView = Backbone.Collection.extend({
+
+    //template:_.template($('#').html()),
+
+    initialize:function () {
+    }
+
+    render:function (eventName) {
+    } 
+});
+
+
+window.ForecastView = Backbone.Collection.extend({
+	
+    //template:_.template($('#').html()),
+
+    initialize:function () {
+    },
+
+    render:function (eventName) {
+      	$(this.el).html(this.template(this.model.toJSON()));
+        return this;
+    }
+});
 
 
 var appRouter = Backbone.Router.extend({
@@ -33,10 +94,13 @@ var appRouter = Backbone.Router.extend({
 	},
 
 	displayWeather : function(){
-		//alert("hello");
+		var forecast = new WeatherCollection();
+		this.ForecastView = new ForecastView({model:this.forecast});
+
+		//forecast.fetch();
+        //$('#').html(this.ForecastView.render().el);
 	}
 });
-
 
 var app = new appRouter();
 
