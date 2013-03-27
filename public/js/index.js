548186abe9213b484488f7ce85293a77723dbf50
Rahul Bhattacharya <r3bhatta@gmail.com>
2013-03-26 23:02:28 -0700

    retab try #2

--- public/js/index.js
+++ public/js/index.js
@@ -1,160 +1,160 @@
 
 window.WeatherModel = Backbone.Model.extend({
-	urlRoot : "apis/weather",
-	defaults :{
-		"date"		:null,
-		"day"		:null,
-		"asOf"		:null,
-		"condition"	:null,
-		"icon"		:null,
-		"temp"		:null,
-		"humidity"	:null,
-		"windchill"	:null,
-		"max"		:null,
-		"min"		:null,
-		"wind"		:null,
-		"winddirection"	:null,
-		"precipitation"	:null,
-		"radiation"	:null,
-		"high"		:null,
-		"low"		:null
-	},
+    urlRoot : "apis/weather",
+    defaults :{
+	"date"		:null,
+	"day"		:null,
+	"asOf"		:null,
+	"condition"	:null,
+	"icon"		:null,
+	"temp"		:null,
+	"humidity"	:null,
+	"windchill"	:null,
+	"max"		:null,
+	"min"		:null,
+	"wind"		:null,
+	"winddirection"	:null,
+	"precipitation"	:null,
+	"radiation"	:null,
+	"high"		:null,
+	"low"		:null
+    },
 
-	/*
+    /*
 
-	parse : function() {
-		WeatherModel wm = new WeatherModel({
-			date: reponse.date,
-			day: response.current.day
-		});
-		return wm;
-	}
-	*/
+      parse : function() {
+      WeatherModel wm = new WeatherModel({
+      date: reponse.date,
+      day: response.current.day
+      });
+      return wm;
+      }
+    */
 
-	parse : function(response){
-		return response.response.data.Current;
-	}
+    parse : function(response){
+	return response.response.data.Current;
+    }
 });
 
 window.ForecastModel = Backbone.Model.extend({
-	urlRoot:"apis/weather",
-	defaults:{
-		"condition"	:null,
-		"image"		:null,
-		"high"		:null,
-		"low"		:null  
-	}
+    urlRoot:"apis/weather",
+    defaults:{
+	"condition"	:null,
+	"image"		:null,
+	"high"		:null,
+	"low"		:null  
+    }
 });
 
 window.WeatherCollection = Backbone.Collection.extend({
-	model	:ForecastModel,
-	url	:"apis/weather",
+    model	:ForecastModel,
+    url	:"apis/weather",
 
-	parse:	function(response){
-		return response.response.data.Week.result;
-	}
+    parse:	function(response){
+	return response.response.data.Week.result;
+    }
 });
 
 
 window.ForecastItemView = Backbone.View.extend({
 
-	template:_.template($('#forecast-template').html()),
+    template:_.template($('#forecast-template').html()),
 
-	initialize:function () {
-	},
+    initialize:function () {
+    },
 
-	render:function () {
-		this.$el.html(this.template(this.model.toJSON()));
-		return this;
-	},
+    render:function () {
+	this.$el.html(this.template(this.model.toJSON()));
+	return this;
+    },
 
-	close:function(){
-		this.$el.remove();
-	}
+    close:function(){
+	this.$el.remove();
+    }
 });
 
 window.ForecastView = Backbone.View.extend({
 
-	initialize:function () {
-	},
+    initialize:function () {
+    },
 
-	render:function () {
-		_.each(this.model.models, function (forecast) {
-			this.$el.append(new ForecastItemView({model:forecast}).render().el);
-		}, this);
-		return this;
-	},
+    render:function () {
+	_.each(this.model.models, function (forecast) {
+	    this.$el.append(new ForecastItemView({model:forecast}).render().el);
+	}, this);
+	return this;
+    },
 
-	close:function(){
-		this.$el.empty();
-	}
+    close:function(){
+	this.$el.empty();
+    }
 });
 
 
 window.GraphView = Backbone.View.extend({
 
-	//template:_.template($('#').html()),
+    //template:_.template($('#').html()),
 
-	initialize:function () {
-	},
+    initialize:function () {
+    },
 
-	render:function (eventName) {
-	} 
+    render:function (eventName) {
+    } 
 });
 
 window.WeatherView = Backbone.View.extend({
 
-	template:_.template($('#weather-template').html()),
+    template:_.template($('#weather-template').html()),
 
-	initialize:function () {
-	},
+    initialize:function () {
+    },
 
-	render:function (eventName) {
-		this.$el.html(this.template(this.model.toJSON()));
-		return this;
-	} 
+    render:function (eventName) {
+	this.$el.html(this.template(this.model.toJSON()));
+	return this;
+    } 
 });
 
 window.WeatherDetailsView = Backbone.View.extend({
 
-	template:_.template($('#weather-details-template').html()),
+    template:_.template($('#weather-details-template').html()),
 
-	initialize:function () {
-	},
+    initialize:function () {
+    },
 
-	render:function (eventName) {
-		this.$el.html(this.template(this.model.toJSON()));
-		return this;
-	} 
+    render:function (eventName) {
+	this.$el.html(this.template(this.model.toJSON()));
+	return this;
+    } 
 });
 
 var appRouter = Backbone.Router.extend({
-	routes: {
-		"" : "displayWeather"
-	},
+    routes: {
+	"" : "displayWeather"
+    },
 
-	displayWeather : function(){
+    displayWeather : function(){
 
-		var weather 		= new WeatherModel();
-		var forecast 		= new WeatherCollection();
-		var weatherView 	= new WeatherView({model:weather});
-		var weatherDetailsView 	= new WeatherDetailsView({model:weather});
-		var forecastView 	= new ForecastView({model:forecast});
+	var weather 		= new WeatherModel();
+	var forecast 		= new WeatherCollection();
+	var weatherView 	= new WeatherView({model:weather});
+	var weatherDetailsView 	= new WeatherDetailsView({model:weather});
+	var forecastView 	= new ForecastView({model:forecast});
 
 
-		forecast.fetch({ 
-			success: function(){
-				$('#forecast').html(forecastView.render().el);
-			}
-		});
+	forecast.fetch({ 
+	    success: function(){
+		$('#forecast').html(forecastView.render().el);
+	    }
+	});
 
-		weather.fetch({
-			success: function(){
-				$('#weather').html(weatherView.render().el);
-				$('#weatherdetails').html(weatherDetailsView.render().el);
-			}
-		})
-	}
+	weather.fetch({
+	    success: function(){
+		$('#weather').html(weatherView.render().el);
+		$('#weatherdetails').html(weatherDetailsView.render().el);
+	    }
+	})
+    }
 });
 
 var app = new appRouter();
